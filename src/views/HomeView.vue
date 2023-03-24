3fc51dd68466c33bc03452d8df2fce2282d81655
Murhussain <husseinmurashi@yahoo.com>
2023-03-24 13:51:33 +0200

    Learning composition api with setup in script

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+
+let message = $ref("Hello World!");
+
+setTimeout(() => {
+  message = "Hello i am changed";
+}, 2000);
 </script>
 
 <template>
   <main>
     <TheWelcome />
+
+    <p>{{message}}</p>
+    <p>
+      <input type="text" v-model="message">
+    </p>
   </main>
 </template>
